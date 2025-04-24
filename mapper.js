dd0fdbb61decd9091493e40d10cef77a4332a1f6
Priyanshu <priyanshu.singh@contentstack.com>
2025-04-24 15:52:57 +0530

    Add new actions to actionMapper and fix trailing commas

--- mapper.js
+++ mapper.js
@@ -8,7 +8,7 @@ const actionMapper = {
       include_branch: "include_branch",
     },
     params: {
-      content_type_name: "content_type_name"
+      content_type_name: "content_type_name",
     },
   },
   "Delete an Entry": {
@@ -20,7 +20,7 @@ const actionMapper = {
     },
     params: {
       content_type_name: "content_type_name",
-      entry_id: "entry_id"
+      entry_id: "entry_id",
     },
   },
   "Get All Entries": {
@@ -51,7 +51,7 @@ const actionMapper = {
       skip: "skip",
       include_count: "include_count",
       include_branch: "include_branch",
-    }
+    },
   },
   "Get an Entry": {
     apiUrl: `/v3/content_types/content_type_name/entries/entry_id`,
@@ -62,6 +62,114 @@ const actionMapper = {
       locale: "locale",
       include_workflow: "include_workflow",
       include_publish_details: "include_publish_details",
+      include_branch: "include_branch",
+    },
+    params: {
+      content_type_name: "content_type_name",
+      entry_id: "entry_id",
+    },
+  },
+  "Add Items to Release": {
+    apiUrl: `/v3/releases/release_id/items`,
+    method: "POST",
+    body: "item_data",
+    queryParams: {
+      include_branch: "include_branch",
+    },
+    params: {
+      release_id: "release_id",
+    },
+  },
+  "Get All Global Fields": {
+    apiUrl: `/v3/global_fields`,
+    method: "GET",
+    queryParams: {
+      include_branch: "include_branch",
+    },
+  },
+  "Get All Taxonomies": {
+    apiUrl: `/v3/taxonomies`,
+    method: "GET",
+    queryParams: {
+      limit: "limit",
+      skip: "skip",
+      include_count: "include_count",
+      include_terms_count: "include_terms_count",
+      include_referenced_terms_count: "include_referenced_terms_count",
+      include_referenced_entries_count: "include_referenced_entries_count",
+      deleted: "deleted",
+      typeahead: "typeahead",
+      query: "query",
+    },
+  },
+  "Get All Variants of an Entry": {
+    apiUrl: `/v3/content_types/content_type_name/entries/entry_uid/variants`,
+    method: "GET",
+    queryParams: {
+      locale: "locale",
+      include_branch: "include_branch",
+    },
+    params: {
+      content_type_name: "content_type_name",
+      entry_uid: "entry_uid",
+    },
+  },
+
+  "Get a Single Asset": {
+    apiUrl: `/v3/assets/asset_id`,
+    method: "GET",
+    queryParams: {
+      version: "version",
+      environment: "environment",
+      include_branch: "include_branch",
+    },
+    params: {
+      asset_id: "asset_id",
+    },
+  },
+
+  "Get All Content Types": {
+    apiUrl: `/v3/content_types`,
+    method: "GET",
+    queryParams: {
+      limit: "limit",
+      skip: "skip",
+      include_count: "include_count",
+      include_branch: "include_branch",
+      include_global_field_schema: "include_global_field_schema",
+    },
+  },
+
+  "Get a Single Variant": {
+    apiUrl: `/v3/variant_groups/variant_group_uid/variants/variant_id`,
+    method: "GET",
+    queryParams: {
+      include_branch: "include_branch",
+    },
+    params: {
+      variant_group_uid: "variant_group_uid",
+      variant_id: "variant_id",
+    },
+  },
+  "Publish an Entry": {
+    apiUrl: `/v3/content_types/content_type_name/entries/entry_id/publish`,
+    method: "POST",
+    body: "body",
+    queryParams: {
+      locale: "locale",
+      environments: "environments",
+      include_branch: "include_branch",
+    },
+    params: {
+      content_type_name: "content_type_name",
+      entry_id: "entry_id",
+    },
+  },
+  "Unlocalize an Entry": {
+    apiUrl: `/v3/content_types/content_type_name/entries/entry_id/unlocalize`,
+    method: "POST",
+    queryParams: {
+      locale: "locale",
       include_branch: "include_branch",
     },
     params: {
